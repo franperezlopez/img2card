16688b0e84e79435f118f8dd4885ab4c991ba624
franperezlopez <1222398+franperezlopez@users.noreply.github.com>
2024-01-02 16:32:48 +0100

    langchain support (#5)
    * ✨ Refactor main classes and functions
    * add langchain and langsmith support

--- src/llm/prompt.py
+++ src/llm/prompt.py
@@ -1,4 +1,5 @@
 
-VISION = "if image type is photo, return venue name (usually, bigger text) and venue type. if image type is card, captionize elements in the image. always return the answer with json format."
+VISION_TOOL = "if image type is a photo, return venue name (usually, bigger text) and venue type (make your best bet). if image type is card, return captionized elements in the image. always return the response in json format. do not make up data."
 
-AGENT = "convert previous json data to document type RFC 6350 using text representation. ignore logo and other decorations. be precise and concise. do not explain your output"
+AGENT_SYSTEM = "you are an expert in vCard format"
+AGENT_TOOL = "convert previous json data to document type RFC 6350 using text representation. ignore logo and other decorations. be precise and concise. do not explain your output"
--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,11 @@
-from typing import Optional, Union
-from pydantic import Field
-from pydantic_settings import BaseSettings
-from functools import cache
 import uuid
+from functools import cache, cached_property
+from typing import Optional, Union
+
+from langchain.callbacks import LangChainTracer
+from pydantic import Field, computed_field
+from pydantic_settings import BaseSettings
+
 
 class Settings(BaseSettings):
     class Config:
@@ -12,7 +15,7 @@ class Settings(BaseSettings):
         case_sensitive = True
         extra = "ignore"
         # allow_mutation = False
-    
+
     AZURE_OPENAI_API_KEY: str = Field(env="AZURE_OPENAI_API_KEY")
     AZURE_OPENAI_API_BASE: str = Field(env="AZURE_OPENAI_API_BASE")
     AZURE_OPENAI_API_VERSION: str = Field(default="2023-07-01-preview", env="AZURE_OPENAI_API_VERSION")
@@ -24,17 +27,33 @@ class Settings(BaseSettings):
 
     SERPAPI_API_KEY: Optional[str] = Field(default=None, env="SERPAPI_API_KEY")
 
-    LANGCHAIN_TRACING_V2: str = Field(default="true", env="LANGCHAIN_TRACING_V2")
-    LANGCHAIN_ENDPOINT: str = Field(default="https://api.smith.langchain.com", env="LANGCHAIN_ENDPOINT")
-    LANGCHAIN_API_KEY: Optional[str] = Field(default=None, env="LANGCHAIN_API_KEY")
-    LANGCHAIN_PROJECT: str = Field(default="img2card", env="LANGCHAIN_PROJECT")
+    LANGSMITH_ENDPOINT: str = Field(default="https://api.smith.langchain.com", env="LANGSMITH_ENDPOINT")
+    LANGSMITH_API_KEY: Optional[str] = Field(default=None, env="LANGSMITH_API_KEY")
+    LANGSMITH_PROJECT: str = Field(default="img2card", env="LANGSMITH_PROJECT")
 
     TELEGRAM_TOKEN: str = Field(env="TELEGRAM_TOKEN")
-    TELEGRAM_SECRET: Optional[str] = Field(default_factory=lambda : str(uuid.uuid4()).replace('-', ''), env="TELEGRAM_SECRET")
+    TELEGRAM_SECRET: Optional[str] = Field(
+        default_factory=lambda: str(uuid.uuid4()).replace("-", ""), env="TELEGRAM_SECRET"
+    )
     TELEGRAM_WEBHOOK_URL: Optional[str] = Field(default=None, env="TELEGRAM_WEBHOOK_URL")
     TELEGRAM_DEV_CHAT_ID: Optional[Union[int, str]] = Field(default=None, env="TELEGRAM_DEV_CHAT_ID")
+
+    @computed_field
+    @cached_property
+    def LANGSMITH_TRACER(self) -> Optional[LangChainTracer]:
+        return get_langsmith_tracer(self)
 
 
 @cache
 def get_settings() -> Settings:
     return Settings()
+
+
+def get_langsmith_tracer(settings) -> Optional[LangChainTracer]:
+    if settings.LANGSMITH_API_KEY:
+        from langsmith import Client
+        return LangChainTracer(
+            project_name=settings.LANGSMITH_PROJECT,
+            client=Client(api_url=settings.LANGSMITH_ENDPOINT, api_key=settings.LANGSMITH_API_KEY),
+        )
+    return None
